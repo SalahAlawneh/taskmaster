76c871f28a78b02a296aecff517b10e2c3d0adea
SalahAlawneh <salah.alawneh@yahoo.com>
2021-05-19 06:45:49 +0300

    finish lab26

--- app/src/main/java/com/salah/taskmaster/MainActivity.java
+++ app/src/main/java/com/salah/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.salah.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTaskButt = MainActivity.this.findViewById(R.id.add_task_button);
+        addTaskButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddTask = new Intent(MainActivity.this, AddTask.class);
+//                goToAddTask.putExtra("key","value");
+                MainActivity.this.startActivity(goToAddTask);
+            }
+        });
+
+
+        Button allTasksButt = MainActivity.this.findViewById(R.id.all_tasks_button);
+        allTasksButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAllTasks = new Intent(MainActivity.this, AllTasks.class);
+                MainActivity.this.startActivity(goToAllTasks);
+            }
+        });
     }
+
 }
